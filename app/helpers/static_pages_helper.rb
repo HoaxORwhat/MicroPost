2e722014235a6aa96f15f320d2770add61c7d482
HoaxORwhat <khalidhedaya98@gmail.com>
2023-07-17 12:32:11 +0300

    Finish layout and routes

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -1,2 +1,10 @@
 module StaticPagesHelper
+    def full_title(title = '')
+        base = 'Sample App'
+        if(title.empty?)
+            base
+        else
+            title + ' | ' + base 
+        end
+    end
 end
